0b262a19b435686278ca363fb0e08961e63b2ccb
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-10-22 14:11:41 +0530

    update need-analysis-form -> responsive and useState for Calculation part

--- src/app/form/step3/page.jsx
+++ src/app/form/step3/page.jsx
@@ -1,13 +1,141 @@
 "use client";
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import FormContainer from "@/components/FormContainer";
 import NeedAnalysisFormHeader from "@/components/NeedAnalysisFormHeader";
 import ProgressBar from "@/components/ProgressBar";
 import { Button } from "@/components/ui/button";
+import FormNavButton from "@/components/FormNavButton";
+import { useRouter } from "next/navigation";
 
 export default function Form3Page() {
+
+    const router = useRouter();
+    const handleBack = () => router.push("/form/step2");
+
+    const [isMobile , setIsMobile] = useState(false);
+    const [isValue , setIsValue]  = useState(0)
+
+    useEffect(() => {
+      const checkMobile = () => setIsMobile(window.innerWidth < 768);
+
+      checkMobile();
+      window.addEventListener("resize", checkMobile);
+
+      return () => window.removeEventListener("resize", checkMobile);
+    }, []);
+
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col">
+
+      {/* Mobile view */}
+
+      {isMobile ? (
+      
+      <div>
+
+      <ProgressBar currentStep={3} totalSteps={4} />
+
+      <section className="flex-grow flex justify-center items-start py-6 px-4">
+        
+      <FormContainer>
+        <div className="mb-6">
+          <p className="font-bold text-2xl">Calculation of Life Cover</p>
+          <p className="text-neutral-500">
+            Enter Your Details in the Following Fields
+          </p>
+        </div>
+          
+        <form className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8 text-sm">
+          {/* Left column */}
+          <div className="flex flex-col space-y-4">
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Fixed Monthly Expenses
+              </label>
+              <input
+                type="text"
+                className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
+              />
+            </div>
+
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Bank Interest Rate
+              </label>
+              <input
+                type="text"
+                className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
+              />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <label className="block text-gray-700 font-medium mb-1">
+                Your Human Life Value
+              </label>
+              <div className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-60 text-white flex items-center justify-center font-bold text-2xl">{isValue}</div>
+            </div>
+          </div>
+
+          {/* Right column */}
+          <div className="flex flex-col space-y-4">
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Unsecured Bank Loan
+              </label>
+              <input
+                type="text"
+                placeholder="optional"
+                className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
+              />
+            </div>
+
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Cash In Hand + Insurance
+              </label>
+              <input
+                type="text"
+                placeholder="optional"
+                className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
+              />
+            </div>
+
+            <div className="flex flex-col items-center">
+              <label className="block text-gray-700 font-medium mb-1">
+                Your Actual Human Life Value
+              </label>
+              <div className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-60 text-white flex items-center justify-center font-bold text-2xl">{isValue}</div>
+            </div>
+          </div>
+        </form>
+
+        {/* Navigation Buttons */}
+        <div className="flex flex-col sm:flex-row justify-between items-center mt-8 gap-2 sm:gap-0">
+          <FormNavButton
+            label="Back"
+            type="prev"
+            variant="gradient"
+            onClick={handleBack}
+          />
+          {/* <FormNavButton
+            label="Next"
+            type="next"
+            variant="gradient"
+            onClick={handleNext}
+          /> */}
+        </div>
+      </FormContainer>
+
+      </section>
+      </div>
+
+      ) : (
+
+      // desktop view
+
+      <div>
+
       <NeedAnalysisFormHeader />
       <ProgressBar currentStep={3} totalSteps={4} />
 
@@ -19,33 +147,31 @@ export default function Form3Page() {
               Enter Your Details in the Following Fields
             </p>
           </div>
-
+            
           <form className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8 text-sm">
             {/* Left column */}
             <div className="flex flex-col space-y-4">
-              <div>
+              <div className="order-1">
                 <label className="block text-gray-700 font-medium mb-1">
                   Fixed Monthly Expenses
                 </label>
                 <input
                   type="text"
-                  placeholder=""
                   className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
                 />
               </div>
 
-              <div>
+              <div className="order-2">
                 <label className="block text-gray-700 font-medium mb-1">
                   Bank Interest Rate
                 </label>
                 <input
                   type="text"
-                  placeholder=""
                   className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#DCE7F2] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
                 />
               </div>
 
-              <div>
+              <div className="order-4 md:order-3">
                 <label className="block text-gray-700 font-medium mb-1">
                   Unsecured Bank Loan
                 </label>
@@ -56,7 +182,7 @@ export default function Form3Page() {
                 />
               </div>
 
-              <div>
+              <div className="order-5 md:order-4">
                 <label className="block text-gray-700 font-medium mb-1">
                   Cash In Hand + Insurance
                 </label>
@@ -69,39 +195,45 @@ export default function Form3Page() {
             </div>
 
             {/* Right column */}
-            <div className="flex flex-col space-y-4">
-              <div>
+            <div className="flex flex-col space-y-4 mt-7">
+              <div className="order-2 md:order-1">
                 <label className="block text-gray-700 font-medium mb-1">
                   Your Human Life Value
                 </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
-                />
+                <div className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-full text-white flex items-center justify-center font-bold text-2xl">{isValue}</div>
               </div>
 
-              <div>
-                <label className="block text-gray-700 font-medium mb-1">
+              <div className="order-2 md:order-2 mt-20">
+                <label className="block text-gray-700 font-medium">
                   Your Actual Human Life Value
                 </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-full focus:outline-none focus:ring-2 focus:ring-[#8EABD2]"
-                />
+                <div className="border border-[#8EABD2] rounded-full px-4 py-2 bg-[#7792b7] w-full text-white flex items-center justify-center font-bold text-2xl">{isValue}</div>
               </div>
             </div>
           </form>
 
-          {/* Submit button */}
-          <div className="flex mt-8 justify-center">
-            <Button className="w-32 rounded-full py-3" variant="gradient">
-              Submit
-            </Button>
-          </div>
+          {/* Navigation Buttons */}
+          <div className="flex flex-col sm:flex-row justify-between items-center mt-8 gap-2 sm:gap-0">
+              <FormNavButton
+                        label="Back"
+                        type="prev"
+                        variant="gradient"
+                        onClick={handleBack}
+              />
+               <FormNavButton
+                        label="Back"
+                        type="prev"
+                        variant="gradient"
+                        onClick={handleBack}
+              />
+  
+              </div>
         </FormContainer>
       </section>
+      </div>
+
+      )}
+      
     </main>
   );
-}
+} 
