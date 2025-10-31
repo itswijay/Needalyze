23481d813d5d0636e40bf2e654f47783d421983a
ShehaanN <shehannadeesha2580@gmail.com>
2025-11-01 03:13:55 +0530

    feat: linked the createLinkDialog  to the dashboard

--- src/app/dashboard/components/CreateLinkDialog.jsx
+++ src/app/dashboard/components/CreateLinkDialog.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -10,73 +10,70 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useState } from "react";
 
 const CreateLinkDialog = () => {
-    const [link, setLink] = useState(
+  const [link, setLink] = useState(
     "https://formlink.com/s/your-generated-link"
-  );
-const [copied, setCopied] = useState(false);
+  );
+  const [copied, setCopied] = useState(false);
   return (
     <div>
       <Dialog>
-            <DialogTrigger asChild>
-              <Button className="bg-gradient-to-r from-[#3EAA66] to-[#189370] px-4 py-5">
-                Create New Link
+        <DialogTrigger asChild>
+          <Button className="bg-gradient-to-r from-[#3EAA66] to-[#189370] w-full px-2 py-5">
+            Create New Link
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Create link</DialogTitle>
+            <DialogDescription>
+              Copy this link to share view access with others.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center gap-2">
+            <div className="grid flex-1 gap-2">
+              <Label htmlFor="link" className="sr-only">
+                Link
+              </Label>
+              <Input id="link" value={link} readOnly />
+            </div>
+          </div>
+          <DialogFooter className="inline-flexflex sm:justify-between ">
+            <div className="hidden sm:flex">
+              <DialogClose asChild>
+                <Button type="button" variant="outline" className="">
+                  Close
+                </Button>
+              </DialogClose>
+            </div>
+            <div>
+              <Button
+                type="button"
+                variant="primary"
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(link)
+                    .then(() => {
+                      setCopied(true);
+                      setTimeout(() => setCopied(false), 2000);
+                    })
+                    .catch(() => {});
+                }}
+                className="bg-gradient-to-r from-[#3EAA66] to-[#189370] hover:opacity-90 text-white cursor-pointer"
+              >
+                {copied ? "Copied" : "Copy Link"}
               </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-md">
-              <DialogHeader>
-                <DialogTitle>Create link</DialogTitle>
-                <DialogDescription>
-                  Copy this link to share view access with others.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="flex items-center gap-2">
-                <div className="grid flex-1 gap-2">
-                  <Label htmlFor="link" className="sr-only">
-                    Link
-                  </Label>
-                  <Input id="link" value={link} readOnly />
-                </div>
-              </div>
-              <DialogFooter className="flex sm:justify-between ">
-                <div>
-                  <DialogClose asChild>
-                    <Button type="button" variant="outline" className="">
-                      Close
-                    </Button>
-                  </DialogClose>
-                </div>
-                <div>
-                  <Button
-                    type="button"
-                    variant="primary"
-                    onClick={() => {
-                      navigator.clipboard
-                        .writeText(link)
-                        .then(() => {
-                          setCopied(true);
-                          setTimeout(() => setCopied(false), 2000);
-                        })
-                        .catch(() => {
-                         
-                        });
-                    }}
-                    className="bg-gradient-to-r from-[#3EAA66] to-[#189370] hover:opacity-90 text-white cursor-pointer"
-                  >
-                    {copied ? "Copied" : "Copy Link"}
-                  </Button>
-                </div>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default CreateLinkDialog
-
+export default CreateLinkDialog;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,17 +2,16 @@ import Cards from "./components/Cards";
 import { DataTable } from "./components/Dashtable";
 import Navbar from "./components/Navbar";
 import { Button } from "@/components/ui/button";
+import CreateLinkDialog from "./components/CreateLinkDialog";
 
 const page = () => {
   return (
     <div className="bg-[var(--primary-50)]/4">
-      <div className="min-h-screen max-w-[95rem] mx-auto  ">
+      <div className="min-h-screen max-w-7xl p-4 mx-auto  ">
         <Navbar />
         <Cards />
-        <div className=" px-4 mx-auto max-w-7xl flex  justify-end">
-          <Button className="bg-gradient-to-r from-[#3EAA66] to-[#189370] px-4 py-5">
-            Create New Link
-          </Button>
+        <div className=" flex justify-end my-4">
+          <CreateLinkDialog />
         </div>
         <DataTable />
       </div>
