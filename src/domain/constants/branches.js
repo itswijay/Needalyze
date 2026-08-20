7da6eea0c28c392161669fb5a483010e221af6b0
Pubudu Wijesundara <neo17641@gmail.com>
2026-08-20 08:53:30 +0530

    feat: expand BRANCH_OPTIONS to include additional operational locations

--- src/domain/constants/branches.js
+++ src/domain/constants/branches.js
@@ -2,4 +2,12 @@
  * Branches the company operates. Previously an inline `['Warakapola']` literal
  * in both register/page.jsx and dashboard/components/Profile.jsx.
  */
-export const BRANCH_OPTIONS = Object.freeze(['Warakapola'])
+export const BRANCH_OPTIONS = Object.freeze([
+  'Colombo',
+  'Gampaha',
+  'Galle',
+  'Kandy',
+  'Kegalle',
+  'Kurunegala',
+  'Warakapola',
+])
